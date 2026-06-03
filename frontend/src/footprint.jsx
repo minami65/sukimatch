880d6d612ac01ebe2e64b3358f80fcbdc8a4ae96
minami kawanabe <minami.kawanabe@rakus-partners.co.jp>
2026-06-03 17:03:10 +0900

    ログイン機能追加

--- frontend/src/footprint.jsx
+++ frontend/src/footprint.jsx
@@ -3,18 +3,23 @@ import "./styles/footprint.css";
 import "./assets/default-profile.png";
 import { useState, useEffect } from "react";
 import Likes from "./components/likes";
+import { useNavigate } from "react-router-dom";
 
 export default function FootPrint() {
+  const navigate = useNavigate();
   const [footprints, setFootprints] = useState([]);
   const [locations, setLocations] = useState([]);
   const [images, setImages] = useState([]);
+  const token = localStorage.getItem("token");
 
   // 足あと取得
   useEffect(() => {
     const fetchFootprints = async () => {
       try {
-        const token =
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwiZXhwIjoxNzgwMDM1Mjk3fQ.GoudyJero96Ds1Rcc6Avf0Ud5bIyZ1NvNAVRmRqDDYs";
+        if (!token) {
+          navigate("/");
+          return;
+        }
         const response = await fetch(
           "http://127.0.0.1:8000/users/me/footprint",
           {
@@ -31,7 +36,7 @@ export default function FootPrint() {
       }
     };
     fetchFootprints();
-  }, []);
+  }, [navigate, token]);
 
   // 画像取得
   useEffect(() => {
@@ -39,8 +44,10 @@ export default function FootPrint() {
 
     const fetchUserImages = async () => {
       try {
-        const token =
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwiZXhwIjoxNzgwMDM1Mjk3fQ.GoudyJero96Ds1Rcc6Avf0Ud5bIyZ1NvNAVRmRqDDYs";
+        if (!token) {
+          navigate("/");
+          return;
+        }
 
         const imageDataList = await Promise.all(
           footprints.map(async (f) => {
@@ -72,7 +79,7 @@ export default function FootPrint() {
     };
 
     fetchUserImages();
-  }, [footprints]);
+  }, [footprints, navigate, token]);
 
   // 都道府県取得
   useEffect(() => {
@@ -92,34 +99,40 @@ export default function FootPrint() {
     <div>
       <p className="title">あしあと</p>
       {/* 名前 */}
-      {footprints.map((f) => {
-        console.log(f.id);
-        const location = locations.find((l) => l.id === f.current_location_id);
-        return (
-          <div className="footprint-card">
-            {images.map((i) => {
-              if (i.user_id === f.user_id) {
-                return (
-                  <img
-                    src={`http://127.0.0.1:8000${i.images[0].image_url}`}
-                    alt="Profile"
-                    className="profileImg"
-                  />
-                );
-              }
-            })}
-            <div key={f.user_id}>
-              <p>{f.name}</p>
-              <div className="footprint-info">
-                <p>{f.age}歳</p>
-                <p>{location ? location.name : f.current_location_id}</p>
+      {footprints.length > 0 ? (
+        footprints.map((f) => {
+          console.log(f.id);
+          const location = locations.find(
+            (l) => l.id === f.current_location_id,
+          );
+          return (
+            <div className="footprint-card">
+              {images.map((i) => {
+                if (i.user_id === f.user_id) {
+                  return (
+                    <img
+                      src={`http://127.0.0.1:8000${i.images[0].image_url}`}
+                      alt="Profile"
+                      className="profileImg"
+                    />
+                  );
+                }
+              })}
+              <div key={f.user_id}>
+                <p>{f.name}</p>
+                <div className="footprint-info">
+                  <p>{f.age}歳</p>
+                  <p>{location ? location.name : f.current_location_id}</p>
+                </div>
               </div>
+              {/* いいねボタン */}
+              <Likes footprintId={f.user_id} />
             </div>
-            {/* いいねボタン */}
-            <Likes footprintId={f.user_id} />
-          </div>
-        );
-      })}
+          );
+        })
+      ) : (
+        <p className="no-footprints">足あとがありません</p>
+      )}
 
       <PageFooter />
     </div>
