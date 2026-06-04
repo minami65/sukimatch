0d7637e41897b127cbc378011b5d696422e96d32
minami kawanabe <minami.kawanabe@rakus-partners.co.jp>
2026-06-04 14:39:44 +0900

    マッチしたユーザーの取得API追加

--- backend/app/crud/likes.py
+++ backend/app/crud/likes.py
@@ -30,23 +30,23 @@ def create_like(db:Session,from_user_id:int,to_user_id:int):
   db.add(like)
 
   reverse_like = db.query(Likes).filter(
-      Likes.from_user_id == from_user_id,
-      Likes.to_user_id == to_user_id
+      Likes.from_user_id == to_user_id,
+      Likes.to_user_id == from_user_id,
   ).first()
-  
+
   if reverse_like:
       existing_match = db.query(Matches).filter(
-          Matches.user1_id == min(from_user_id,to_user_id),
-          Matches.user2_id == max(from_user_id,to_user_id)
+          Matches.user1_id == min(from_user_id, to_user_id),
+          Matches.user2_id == max(from_user_id, to_user_id),
       ).first()
 
-  if not existing_match:
-      match = Matches(
-            user1_id=min(from_user_id, to_user_id),
-            user2_id=max(from_user_id, to_user_id)
+      if not existing_match:
+          match = Matches(
+              user1_id=min(from_user_id, to_user_id),
+              user2_id=max(from_user_id, to_user_id),
           )
-      db.add(match)
-      
+          db.add(match)
+
   db.commit()
   db.refresh(like)
 
@@ -71,6 +71,22 @@ def get_liked_by_users(db:Session,user_id:int):
         .all()
     )
   return users
+
+# マッチしたユーザー一覧
+def get_my_matches(db: Session, user_id: int):
+  users_as_user1 = (
+      db.query(User)
+      .join(Matches, Matches.user2_id == User.user_id)
+      .filter(Matches.user1_id == user_id)
+      .all()
+  )
+  users_as_user2 = (
+      db.query(User)
+      .join(Matches, Matches.user1_id == User.user_id)
+      .filter(Matches.user2_id == user_id)
+      .all()
+  )
+  return users_as_user1 + users_as_user2
 
 # いいね取り消し
 def delete_like(db:Session,from_user_id:int,to_user_id:int):
