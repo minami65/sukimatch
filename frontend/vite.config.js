35737d1ed724ad269419737b0858058f8b43a9b9
Ryusuke Hirota <ryusuke.hirota@rakus-partners.co.jp>
2026-06-19 13:58:47 +0900

    Setup test environment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,22 @@
-import { defineConfig } from "vite";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import babel from "@rolldown/plugin-babel";
+import { defineConfig } from 'vite';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import babel from '@rolldown/plugin-babel';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
   resolve: {
     alias: {
-      "@": "/src",
+      '@': '/src',
     },
   },
   css: {
     modules: {
-      localsConvention: "camelCase",
+      localsConvention: 'camelCase',
     },
+  },
+  test: {
+    globals: true,
+    environment: 'node',
   },
 });
