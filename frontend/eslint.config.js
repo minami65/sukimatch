c758ca891b404859dc4a117d22e3002dd308aabb
Ryusuke Hirota <ryusuke.hirota@rakus-partners.co.jp>
2026-06-19 17:53:42 +0900

    fix lint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,9 +1,9 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -14,9 +14,15 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        process: 'readonly',
+      },
       parser: tseslint.parser,
       parserOptions: {
         ecmaVersion: 'latest',
@@ -25,8 +31,9 @@ export default defineConfig([
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'quotes': ['error', 'single', { 'avoidEscape': true }]
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['error'],
+      quotes: ['error', 'single', { avoidEscape: true }],
     },
   },
-])
+]);
--- frontend/src/userDetails.jsx
+++ frontend/src/userDetails.jsx
@@ -12,7 +12,6 @@ import {
   PREFECTURES,
   JOB,
   EDUCATION,
-  BODY_TYPE,
   INCOME,
   HOLIDAY,
   ALCOHOL,
