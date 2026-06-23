165e101aa39681d18089f8650188560b8b857fb0
Ryusuke Hirota <ryusuke.hirota@rakus-partners.co.jp>
2026-06-23 11:54:38 +0900

    import順序の設定

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,8 +1,8 @@
 import js from '@eslint/js';
-import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig([
@@ -13,6 +13,7 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      'prettier',
     ],
     plugins: {
       '@typescript-eslint': tseslint.plugin,
