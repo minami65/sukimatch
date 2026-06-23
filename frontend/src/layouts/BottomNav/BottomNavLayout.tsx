2270d97cbde592595b0fbebede8dac2b67729c13
Ryusuke Hirota <ryusuke.hirota@rakus-partners.co.jp>
2026-06-23 14:36:12 +0900

    Create bottom nav layout component

---
+++ frontend/src/layouts/BottomNav/BottomNavLayout.tsx
@@ -0,0 +1,18 @@
+import { Outlet } from 'react-router-dom';
+
+import BottomNav from './BottomNav';
+import styles from './BottomNavLayout.module.css';
+
+function BottomNavLayout() {
+  return (
+    <div className={styles.container}>
+      <main>
+        <Outlet />
+      </main>
+
+      <BottomNav />
+    </div>
+  );
+}
+
+export default BottomNavLayout;
