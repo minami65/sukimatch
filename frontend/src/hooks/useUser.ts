c8ac7f7db705ee3b458f0c9040acc955bcae317f
Ryusuke Hirota <ryusuke.hirota@rakus-partners.co.jp>
2026-06-30 15:03:34 +0900

    ログアウトボタンの作成と使用

---
+++ frontend/src/hooks/useUser.ts
@@ -0,0 +1,46 @@
+import {
+  useGetImagesUsersUserIdImagesGet,
+  useGetUserListUsersGet,
+} from '@/api/generated/endpoints/api';
+import { GetUserListUsersGetParams } from '@/api/generated/models';
+
+// 画像一覧取得
+export const useUserImages = (userId?: number) => {
+  return useGetImagesUsersUserIdImagesGet(userId!, {
+    query: { enabled: !!userId },
+  });
+};
+
+// メイン画像取得
+export const useMainUserImage = (userId?: number) => {
+  const { data, isLoading, error } = useUserImages(userId);
+  const mainImage = data?.find((img) => img.sort_order === 1);
+
+  return {
+    mainImage,
+    isLoading,
+    error,
+  };
+};
+
+// 自分を含めた全てのユーザー一覧
+export const useUserList = (params?: GetUserListUsersGetParams) => {
+  return useGetUserListUsersGet(params);
+};
+
+// 自分を除いた全てのユーザー一覧
+export const useFilteredUsers = (
+  params: GetUserListUsersGetParams | undefined,
+  currentUserId: number,
+) => {
+  const { data, isLoading, error, refetch } = useUserList(params);
+
+  const users = data?.filter((user) => user.user_id !== currentUserId) ?? [];
+
+  return {
+    users,
+    isLoading,
+    error,
+    refetch,
+  };
+};
