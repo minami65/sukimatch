f1b6cf59f895997b77743c3a1c11e6f8074357e4
Ryusuke Hirota <ryusuke.hirota@rakus-partners.co.jp>
2026-07-02 13:59:27 +0900

    Fix check-types

--- frontend/orval.config.js
+++ frontend/orval.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   'sukimatch-api': {
-    input: `http://localhost:8000/openapi.json`,
+    input: `${__dirname}/../backend/openapi.json`,
     output: {
       target: './src/api/generated/endpoints/api.ts',
       schemas: './src/api/generated/models',
