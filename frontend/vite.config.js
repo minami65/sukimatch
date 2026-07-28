168003dac071d432927196bd7e233b449448a876
Ryusuke Hirota <ryusuke.hirota@rakus-partners.co.jp>
2026-07-28 10:00:54 +0900

    その他変更

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
   css: {
     modules: {
       localsConvention: 'camelCase',
+      generateScopedName: '[name]__[local]___[hash:base64:5]',
     },
   },
   test: {
