9c7e4ba16fcef1b66a499fac40548f0fc10aac19
Ryusuke Hirota <ryusuke.hirota@rakus-partners.co.jp>
2026-07-30 16:52:25 +0900

    matchesエンドポイントを修正

--- backend/app/api/matches.py
+++ backend/app/api/matches.py
@@ -1,3 +1,5 @@
+from app.api.deps import get_current_user
+from app.models.user import User
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.db import SessionLocal
@@ -16,23 +18,30 @@ def get_db():
 
 
 @router.get("/matches/me", response_model=list[MatchItem])
-def read_my_matches(db: Session = Depends(get_db)):
-    current_user_id = 1  # ※あとで認証(Auth)ができたら差し替える部分
-    return get_my_matches(db, user_id=current_user_id)
+def read_my_matches(
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+    return get_my_matches(db, user_id=current_user.user_id)
 
 
 @router.get("/matches/me/unread", response_model=list[MatchItem])
-def read_unread_matches(db: Session = Depends(get_db)):
-    current_user_id = 1
-    return get_unread_matches(db, user_id=current_user_id)
+def read_unread_matches(
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+    return get_unread_matches(db, user_id=current_user.user_id)
 
 
 @router.put("/matches/me/read")
 def update_matches_read_status(
     request: MarkReadRequest,
-    db: Session = Depends(get_db)
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
 ):
-    current_user_id = 1
-    mark_matches_as_read(db, user_id=current_user_id,
-                         match_ids=request.match_ids)
+    mark_matches_as_read(
+        db,
+        user_id=current_user.user_id,
+        match_ids=request.match_ids
+    )
     return {"message": "Matches marked as read successfully"}
